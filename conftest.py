5c846986fd674cbac955ac307da7f104ba9d19b3
ARushan@appline.ru <ARushan@appline.ru>
2023-05-09 15:50:51 +0300

    * дописал clear_test_reports_and_logs - дописать кейсы по свагеру - добавить аннотации, удалить лишнее - отредактировать README

--- conftest.py
+++ conftest.py
@@ -20,8 +20,10 @@ def base():
 def clear_test_reports_and_logs():
     """Декоратор по очистке каталогов logs и reports"""
 
-    for f in os.listdir(Config.DIRECTORY_TEST_REPORTS):
-        os.remove(os.path.join(Config.DIRECTORY_TEST_REPORTS, f))
+    f = os.listdir(Config.DIRECTORY_TEST_REPORTS)
+    f.remove('environment.properties')
+    for i in range(0, len(f)):
+        os.remove(f'{Config.DIRECTORY_TEST_REPORTS}/{f[i]}')
 
     for c in os.listdir(Config.DIRECTORY_LOGS):
         os.remove(os.path.join(Config.DIRECTORY_LOGS, c))
